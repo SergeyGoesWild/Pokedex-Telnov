b1994b80970e29e928e2bf4b6f642c383966459d
Sergey Telnov <sergej.telnov@gmail.com>
2024-01-11 15:44:32 +0100

    Step 06 complete

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,52 @@ const pokemonList = [
   },
 
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+
+  {
     name: "mew",
   },
 
 ];
 
 function App() {
-  let pokemon = pokemonList[0]
+
+  let [pokemonIndex, setPokemonIndex] = useState(0);
+
+  let clickIncr = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1)
+    }
+  }
+
+  let clickDecr = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1)
+    }
+  }
+
+  let pokemon = pokemonList[pokemonIndex]
+
   return (
     <div>
       <PokemonCard pokemon={pokemon} />
+      <button onClick={clickDecr} > Previous </button>
+      <button onClick={clickIncr}> Next </button>
     </div>
   );
 
